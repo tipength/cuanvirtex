35b5bbe84cfd01bd82df9077eddb9f55b39f2ebf
TipengOfc <100071150+tipength@users.noreply.github.com>
2022-11-03 17:23:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE DANIXD'
+global.ownername = 'TIPENG REAL'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
-global.packname = 'The Jo Bot'
+global.owner = ['6282136031499','6282136031499','6282136031499']
+global.premium = ['6282136031499']
+global.packname = 'The Tipeng Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
